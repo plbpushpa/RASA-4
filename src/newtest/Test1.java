92528d26129d9a582d8657e39a8305fc43ccfa89
User <User@DESKTOP-ADOVLBI>
2020-08-20 14:27:23 +0530

    second commit

--- src/newtest/Test1.java
+++ src/newtest/Test1.java
@@ -6,6 +6,7 @@ public class Test1 {
 System.out.println("hello world");
 		System.out.println(":hello seljava");
 		System.out.println("this is devops");
+		System.out.println("This is first trip");
 	}
 
 }
